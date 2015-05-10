b7ada559c55952c9dc3f30a4a6360718f1e157b2
Fabian Thoma <fabian@elementaryos.org>
2015-05-10 10:33:15 +0000

    Started with a nodejs implementation of houston

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+
+module.exports = router;
