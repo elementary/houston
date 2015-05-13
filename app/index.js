25b831acfc5f5ffb36e6ccf996ca2879d5e59f68
Fabian Thoma <fabian@elementaryos.org>
2015-05-13 20:48:25 +0200

    Unified config and added sample

--- app/index.js
+++ app/index.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session(config.session));
+app.use(session(config.SESSION));
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Let's get the DB going
--- app/mongodb.js
+++ app/mongodb.js
@@ -2,7 +2,7 @@ var app = require.main.require('./app/index');
 var config = require.main.require('./config');
 
 var mongoose = require('mongoose');
-mongoose.connect(config.mongodbUrl);
+mongoose.connect(config.MONGODB_URL);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
