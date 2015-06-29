68488a15aa13994d05535c6f010987974216ec45
Avi Romanoff <avi@romanoff.me>
2015-06-29 14:17:16 -0700

    refactor/simplify dashboard endpoint, fixes #27

--- app/dashboard.js
+++ app/dashboard.js
@@ -3,31 +3,22 @@ var auth = require.main.require('./app/auth');
 var Hubkit = require('hubkit');
 var _ = require('underscore');
 
-// Create an instance of Hubkit
 var gh = new Hubkit({});
 
-app.get('/dashboard', auth.loggedIn, function(req, res) {
-  // Get the repositories the User has access to
-  var repos = [
-    gh.request('GET /user/repos', {token: req.user.github.accessToken}),
-    gh.request('GET /user/orgs', {token: req.user.github.accessToken})
-      .then(function(orgs) {
-        // Map each Org to their repos
-        return Promise.all(orgs.map(function(org) {
-          return gh.request('GET /orgs/' + org.login + '/repos',
-            {token: req.user.github.accessToken});
-        }));
-      }),
-  ];
-  // Combine these Promises back together and render the output
-  Promise.all(repos).then(function(repos) {
+app.get('/dashboard', auth.loggedIn, function(req, res, next) {
+  // Get the repositories the user owns or is a member of
+  gh.request('GET /user/repos', {
+    token: req.user.github.accessToken,
+    type: 'member',
+  })
+  .then(function(repos) {
     res.render('dashboard', {
       user: req.user,
-      repos: _.compact(_.flatten(repos)),
+      repos: repos,
       title: 'Dashboard',
     });
-  }).catch(function(err) {
-    console.log('Error', err);
-    throw err;
-  });
+  })
+  .catch(function(err) {
+    next(err);
+  })
 });
