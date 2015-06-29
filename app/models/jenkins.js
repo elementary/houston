75980b316e8f806c6983a00e4aa2e909c2dc5b02
Avi Romanoff <avi@romanoff.me>
2015-06-28 22:05:26 -0700

    small style tweaks

--- app/models/jenkins.js
+++ app/models/jenkins.js
@@ -2,18 +2,21 @@ var app = require.main.require('./app');
 var CONFIG = require.main.require('./config');
 var jenkinsClient = require('then-jenkins');
 
-if(CONFIG.JENKINS_ENABLED) jenkins = jenkinsClient(CONFIG.JENKINS_URL);
+if (CONFIG.JENKINS_ENABLED) {
+  jenkins = jenkinsClient(CONFIG.JENKINS_URL);
+}
 
 var Jenkins = {
   doBuild: function(project, params) {
     return jenkins.job.build({
       name: CONFIG.JENKINS_JOB,
-      parameters: params,
+      parameters: params
     });
   },
+
   getLogs: function(build) {
     return jenkins.build.log(CONFIG.JENKINS_JOB, build);
-  },
+  }
 };
 
 // If Jenkins isn't set to be enabled, export an empty object so when somebody
