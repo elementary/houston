c42ca8a47d5231616872db79051e647d4d616487
Fabian Thoma <fabian@elementaryos.org>
2015-07-01 23:07:00 +0200

    Fix the Jenkins HTTP method
    I messed up, this should be a post request

--- app/jenkins-hook.js
+++ app/jenkins-hook.js
@@ -2,7 +2,7 @@ var app = require.main.require('./app');
 var Project = require.main.require('./app/models/project').Model;
 var CONFIG = require.main.require('./config');
 
-app.get('/jenkins-hook/' + CONFIG.JENKINS_SECRET, function(req, res) {
+app.post('/jenkins-hook/' + CONFIG.JENKINS_SECRET, function(req, res) {
   Project.updateBuild(req.body.build)
     .then(function() {
       res.end('ok');
