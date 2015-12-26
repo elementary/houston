dff1de73eb5991b671f55e2184fda27104256065
Avi Romanoff <avi@romanoff.me>
2015-12-25 21:41:33 -0500

    refactor dashboard to use async/await
    this not only improves readability, but it fixes a major problem
    with the previous implementation -- any error that occured inside the promise chain was silently being ignored because of the .reflect() calls. This new code much more accurately represents the actual work being done in this route.

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,5 +1,6 @@
 import Hubkit from 'hubkit';
 import Promise from 'bluebird';
+import _ from 'underscore';
 import ini from 'ini';
 
 import app from '~/';
@@ -9,58 +10,64 @@ import { Application } from '~/models/application';
 // Create Hubkit with a static instance
 let gh = new Hubkit();
 
-app.get('/dashboard', loggedIn, (req, res, next) => {
-  Promise.resolve(gh.request('GET /user/repos', {
-    type: 'public',
-    token: req.user.github.accessToken,
-  }))
-  // Transform the repo results from GitHub into our application format, or load from the DB
-  .map(repoData => findOrCreatefromGitHubData(repoData, req.user))
-  // De-reflect promise
-  .filter(promise => promise.isFulfilled())
-  .map(promise => promise.value())
-  .then(applications => {
-    res.render('dashboard', {
-      title: 'Dashboard',
-      user: req.user,
-      applications,
-    });
-  })
-  .catch(next);
-});
+// wrap for top-level error handling with async functions
+// strongloop.com/strongblog/async-error-handling-expressjs-es7-promises-generators/
+let wrap = fn => (...args) => fn(...args).catch(args[2]);
 
-function findOrCreatefromGitHubData(repoData, user) {
-  return Application.findOne({
-    'github.owner': repoData.owner.login,
-    'github.name':  repoData.name,
-  }).exec()
-  .then(repo => {
-    if (repo) {
-      // Found an existing repo in the Database
-      console.log('Found repo: ' + repo.name);
-      return Promise.resolve(repo)
-        .then(Application.fetchReleases)
-        .then(application => application.save())
-        .reflect();
-    } else {
-      return Promise.resolve(new Application({
-        github: {
-          owner: repoData.owner.login,
-          name: repoData.name,
-          repoUrl: repoData.git_url,
-          APItoken: user.github.accessToken,
-        },
-      }))
-      .then(Application.fetchAppHubFile)
-      .then(Application.parseAppHubFileIfPossible)
-      .then(Application.fetchDesktopFileIfPossible)
-      .then(Application.fetchAppIconIfPossible)
-      .then(Application.fetchReleases)
-      .then(application => {
-        console.log('Created repo: ' + application.name);
-        return application.save();
-      })
-      .reflect();
-    }
+// returns `true` if the repo has a .apphub file
+// in its top-level directory, otherwise returns `false`
+function repoHasAppHubFile(repo, token) {
+  return gh.request(`GET /repos/${repo}/contents/.apphub`, {token})
+  .then(() => true)
+  .catch(err => {
+    // no .apphub file
+    if (err.status == 404) return false;
+    // some other error
+    throw err;
   });
 }
+
+app.get('/dashboard', loggedIn, wrap(async (req, res, next) => {
+  const repos = await gh.request('GET /user/repos', {
+    type: 'public',
+    token: req.user.github.accessToken,
+  });
+
+  const appHubFileResults = await Promise.all(
+    repos.map(repo => repoHasAppHubFile(
+      repo.full_name,
+      req.user.github.accessToken
+    ))
+  );
+
+  const applications = await Promise.all(_.chain(_.zip(repos, appHubFileResults))
+    .filter(([repo, hasAppHubFile]) => hasAppHubFile)
+    .map(([repo, hasAppHubFile]) => {
+      return Application.findOne({
+       'github.owner': repo.owner.login,
+       'github.name': repo.name,
+      })
+      .then(appFromDB => appFromDB || Promise.resolve(new Application({
+          github: {
+            owner: repo.owner.login,
+            name: repo.name,
+            repoUrl: repo.git_url,
+            APItoken: req.user.github.accessToken,
+          },
+        }))
+        .then(Application.parseAppHubFileIfPossible)
+        .then(Application.fetchDesktopFileIfPossible)
+        .then(Application.fetchAppIconIfPossible)
+      )
+      .then(Application.fetchReleases)
+      .then(application => application.save())
+    })
+    .value()
+  );
+
+  res.render('dashboard', {
+    title: 'Dashboard',
+    user: req.user,
+    applications,
+  });
+}));
