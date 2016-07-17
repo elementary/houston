19c05c238cc1376a674dc10c91276788d18cc057
Blake Kostner <btkostner@gmail.com>
2016-07-16 17:37:54 -0700

    trying to clean up aptly + strongback interface

--- src/houston/controller/hook/strongback.js
+++ src/houston/controller/hook/strongback.js
@@ -4,6 +4,7 @@
  */
 
 import _ from 'lodash'
+import Promise from 'bluebird'
 
 import * as aptly from '~/houston/service/aptly'
 import * as github from '~/houston/service/github'
@@ -29,12 +30,13 @@ atc.on('build:start', async (id, data) => {
 
   if (status !== 'QUEUE') {
     log.debug('Received strongback start data for a build already started')
+
     return
   } else {
     log.verbose('Received strongback data for start build')
   }
 
-  build.setStatus('BUILD')
+  return build.setStatus('BUILD')
 })
 
 /**
@@ -47,86 +49,69 @@ atc.on('build:start', async (id, data) => {
  * }
  */
 atc.on('build:finish', async (id, data) => {
-  const cycle = await Cycle.findOne({
-    'builds._id': id
-  })
-  const build = cycle.builds.id(id)
-  const status = await build.getStatus()
-  const project = await Project.findOne(cycle.project)
-
-  if (status !== 'BUILD') {
-    log.debug('Received strongback finish data for a build already built')
-    return
-  } else {
-    log.verbose('Received strongback data for finished build')
-  }
-
-  if (!data.success) {
-    log.debug(`Building ${project.name} failed`)
-    const issue = render('houston/views/issue/build.md', { dist: build.dist, arch: build.arch, log: data.files.log })
-
-    if (data.files != null && data.files.log != null) {
-      log.debug('Saving log file')
-      build.setFile('deb', data.files.log)
-    }
-
-    build.setStatus('FAIL')
-    .then(() => project.postIssue(issue))
-    return
-  }
-
-  build.setStatus('FINISH')
-  .then(async () => {
-    if (data.files == null || data.files.deb == null) return
-
+  return new Promise((resolve, reject) => {
     const cycle = await Cycle.findOne({
       'builds._id': id
     })
     const build = cycle.builds.id(id)
 
+    const project = await Project.findOne(cycle.project)
     const release = project.releases.find((x) => x.version === cycle.version)
 
-    return github.sendFile(project.github.owner, project.github.name, release.github.id, project.github.token, data.files.deb, {
-      content: 'application/vnd.debian.binary-package',
-      name: `${project.name}_${cycle.tag}_${build.arch}.deb`,
-      label: `apphub ${build.arch} (deb)`
-    })
-    .catch((error) => {
-      log.error('Unable to post debian package to GitHub', error)
-    })
-    .then(() => aptly.upload(project.package.name, build._id, data.files.deb))
-  })
-  .then(async () => {
-    const cycle = await Cycle.findOne({
-      'builds._id': id
-    })
-    const status = await cycle.getStatus()
+    const status = await build.getStatus()
 
-    if (cycle._status !== 'DEFER' || status !== 'FINISH') return Promise.resolve()
+    if (status !== 'BUILD') {
+      log.debug('Received strongback finish data for a build already built')
 
-    // All builds have completed but we have yet to set the next cycle status
-    return cycle.setStatus('REVIEW')
-    .then(() => {
-      const buildIds = cycle.builds.map((build) => build.id)
-      const dists = _.uniq(cycle.builds.map((build) => build.dist))
-
-      return aptly.review(project.package.name, cycle.version, buildIds, dists)
-      .then((packages) => cycle.update({ packages }))
-      .catch((error) => {
-        throw new Mistake(500, 'Unable to run aptly review process', error)
-      })
-    })
-  })
-  .catch((error) => {
-    log.error('Error while trying to process strongback finish', error)
-
-    if (data.files != null && data.files.deb != null) {
-      log.debug('Saving deb package')
-      build.setFile('deb', data.files.deb)
+      return resolve()
+    } else {
+      log.verbose('Received strongback data for finished build')
     }
 
-    return cycle.setStatus('ERROR')
-    .then(() => cycle.update({ mistake: error }))
+    if (!data.success) {
+      log.debug(`Building ${project.name} failed`)
+      const issue = render('houston/views/issue/build.md', { dist: build.dist, arch: build.arch, log: data.files.log })
+
+      if (data.files != null && data.files.log != null) {
+        log.debug('Saving log file')
+        await build.setFile('log', data.files.log)
+      }
+
+      await build.setStatus('FAIL')
+      return project.postIssue(issue)
+    }
+
+    await build.setStatus('FINISH')
+
+    if (data.files != null || data.files.deb != null) {
+      await github.sendFile(project.github.owner, project.github.name, release.github.id, project.github.token, data.files.deb, {
+        content: 'application/vnd.debian.binary-package',
+        name: `${project.name}_${cycle.tag}_${build.arch}.deb`,
+        label: `apphub ${build.arch} (deb)`
+      })
+      .catch((error) => {
+        log.error('Unable to post debian package to GitHub', error)
+      })
+
+      await aptly.upload(project.name, build.arch, cycle.version, data.files.deb)
+    }
+
+    // All builds have completed so we set the cycle to review
+    if (cycle._status !== 'DEFER' || status !== 'FINISH') return resolve()
+
+    await cycle.setStatus('REVIEW')
+
+    const archs = _.uniq(cycle.builds.map((build) => build.arch))
+    const dists = _.uniq(cycle.builds.map((build) => build.dist))
+
+    const packages = await aptly.review(project.name, cycle.version, archs, dists)
+    return cycle.update({ packages })
+  })
+  .catch(async (error) => {
+    log.error('Error while trying to process strongback finish', error)
+
+    await cycle.setStatus('ERROR')
+    return cycle.update({ mistake: error })
   })
 })
 
@@ -145,11 +130,12 @@ atc.on('build:error', async (id, error) => {
 
   if (status !== 'BUILD') {
     log.debug('Received strongback error data for a build already built')
+
     return
   } else {
     log.verbose('Received strongback data for build error')
   }
 
-  build.setStatus('ERROR')
-  .then(() => build.update({ mistake: error }))
+  await build.setStatus('ERROR')
+  return build.update({ mistake: error })
 })
