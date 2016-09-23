b87ed1844aa35d13d1892d57033aa982e5086f28
Blake Kostner <btkostner@gmail.com>
2016-09-22 17:35:51 -0700

    inital generateJWT function

--- src/service/github.js
+++ src/service/github.js
@@ -5,9 +5,13 @@
  * @exports {Class} ServiceError - error related to communication with GitHub
  */
 
+import fs from 'fs'
+import jwt from 'jsonwebtoken'
 import moment from 'moment'
 
 import * as service from './index'
+import config from 'lib/config'
+import log from 'lib/log'
 
 /**
  * githubGitHubErrorError
@@ -36,8 +40,35 @@ export class GitHubError extends service.ServiceError {
  * @param {Date} exp - date to expire on
  * @returns {String} - JWT bearer token to authenticate with
  */
-export function generateJWT (exp = moment().add(1, 'hours').toDate()) {
-  if (exp.toDate == null) throw new GitHubError('Unable to generate JWT without expiration date')
+export async function generateJWT (exp = moment().add(2, 'minutes').toDate()) {
+  if (exp.toDate == null) {
+    log.warn('GitHub service tried generating JWT without an expiration date')
+    throw new GitHubError('Unable to generate JWT without expiration date')
+  }
+  if (typeof config.github.integration.id === 'number') {
+    log.warn('GitHub configuration does not include an integration id')
+    throw new GitHubError('Unable to generate JWT without integration id')
+  }
 
+  const key = await new Promise((resolve, reject) => {
+    log.debug('GitHub service is reading integration key')
+    fs.readFile(config.github.integration.key, (err, data) => {
+      if (err) return reject(err)
+      return resolve(data)
+    })
+  })
 
+  const payload = {
+    iat: new Date().getTime(),
+    exp: exp.getTime(),
+    iss: config.github.integration.id
+  }
+
+  return new Promise((resolve, reject) => {
+    log.debug('GitHub service is generating a new JWT token')
+    jwt.sign(payload, key, 'RS256', (err, token) => {
+      if (err) return reject(err)
+      return resolve(token)
+    })
+  })
 }
--- test/service/github.js
+++ test/service/github.js
@@ -4,16 +4,29 @@
  */
 
 import test from 'ava'
+import mock from 'mock-require'
+import path from 'path'
 
-import * as github from 'service/github'
+import alias from 'root/.alias'
+import mockConfig from 'test/fixtures/config'
+
+test.beforeEach((t) => {
+  mock(path.resolve(alias.resolve.alias['root'], 'config'), mockConfig)
+
+  t.context.github = require(path.resolve(alias.resolve.alias['service'], 'github'))
+})
 
 test('GitHubError is an error', (t) => {
+  const github = t.context.github
+
   const one = new github.GitHubError('testing')
 
   t.true(one instanceof Error)
 })
 
 test('GitHubError has correct error code', (t) => {
+  const github = t.context.github
+
   const one = new github.GitHubError('testing')
 
   t.is(one.code, 'GTHERR')
