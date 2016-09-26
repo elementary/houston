7bcba408dcef5dfe03649c42c8a4bdb422b50dc1
Blake Kostner <btkostner@gmail.com>
2016-09-26 12:45:49 -0700

    add basic atc library

--- src/lib/atc.js
+++ src/lib/atc.js
@@ -1,4 +1,115 @@
 /**
  * lib/atc.js
  * A message queue class based around mongodb
+ *
+ * @exports {AtcError} AtcError - An atc specific error class
+ * @exports {Class} Worker - Works on items in a queue
+ * @exports {Class} Sender - Adds items to worker queue
  */
+
+import monq from 'monq'
+
+import config from 'lib/config'
+
+const client = monq(config.database)
+
+/**
+ * AtcError
+ * An atc specific error class
+ *
+ * @extends Error
+ */
+export class AtcError extends Error {
+
+  /**
+   * Creates a new AtcError
+   *
+   * @param {String} msg - error message
+   */
+  constructor (msg) {
+    super(msg)
+
+    this.code = 'ATCERR'
+  }
+}
+
+/**
+ * Worker
+ * Works on items in a queue
+ *
+ * @extends monq.Worker
+ *
+ * @emits Worker#dequeued
+ * @emits Worker#failed
+ * @emits Worker#complete
+ * @emits Worker#error
+ */
+export class Worker extends client.Worker {
+
+  /**
+   * Creates a queue worker
+   *
+   * @param {String} name - Message queue name
+   * @param {Number} int - Time to check for new items in queue
+   */
+  constructor (name, int = 5000) {
+    super([name], {
+      collection: 'queue',
+      interval: int
+    })
+  }
+
+  /**
+   * register
+   * Registers a function for a particular job type
+   *
+   * @param {String} name - job name
+   * @param {Function} fn - function to run given params, must return promise
+   * @return {Void}
+   */
+  register (name, fn) {
+    super({
+      [name]: (param, callback) => {
+        fn(param)
+        .then((val) => callback(null, val))
+        .catch((err) => callback(err))
+      }
+    })
+  }
+}
+
+/**
+ * Sender
+ * Adds items to worker queue
+ */
+export class Sender extends client.Queue {
+
+  /**
+   * Creates a new queue sender
+   *
+   * @param {String} name - Message queue name
+   */
+  constructor (name) {
+    super(client, name, {
+      collection: 'queue'
+    })
+  }
+
+  /**
+   * enqueue
+   * Adds an item to the queue
+   *
+   * @param {String} name - job name
+   * @param {Object} param - job parameters
+   * @param {Object} opt - options
+   * @return {Object} - monq job object
+   */
+  enqueue (name, param, opt) {
+    return new Promise((resolve, reject) => {
+      super(name, param, opt, (err, job) => {
+        if (err) return reject(err)
+        return resolve(job)
+      })
+    })
+  }
+}
