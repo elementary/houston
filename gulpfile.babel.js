5c428654062feda5fe28f9a15ea9facd77f67614
Blake Kostner <btkostner@gmail.com>
2016-10-16 15:58:15 -0700

    Webpack (#255)
    * add postcss processing to gulp
    
    * add webpack configuration
    
    * add script to layout file
    
    * update configuration to new weird webpack 2 config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,22 @@
  * Transcodes and builds all needed files.
  */
 
-import babel from 'gulp-babel'
 import del from 'del'
 import gulp from 'gulp'
 import path from 'path'
+
+import babel from 'gulp-babel'
+import cssnext from 'postcss-cssnext'
+import gulpWebpack from 'gulp-webpack'
+import postcss from 'gulp-postcss'
+import webpack from 'webpack'
+
+import webpackConfig from './webpack.babel.js'
+
+const browsers = [
+  'last 4 version',
+  'not ie <= 11'
+]
 
 /**
  * functionCopy
@@ -16,7 +28,7 @@ import path from 'path'
  * @returns {Stream} - a gulp stream
  */
 const functionCopy = (file) => {
-  const buildFiles = (file == null) ? ['src/**/*', '!src/**/*.js'] : file
+  const buildFiles = (file == null) ? ['src/**/*', '!src/**/*.js', '!src/**/*.css'] : file
 
   return gulp.src(buildFiles, { base: 'src' })
   .pipe(gulp.dest('build'))
@@ -30,11 +42,40 @@ const functionCopy = (file) => {
  * @returns {Stream} - a gulp stream
  */
 const functionBabel = (file) => {
-  const buildFiles = (file == null) ? ['src/**/*.js'] : file
+  const buildFiles = (file == null) ? ['src/**/*.js', '!src/houston/public/scripts/app.js'] : file
 
   return gulp.src(buildFiles, { base: 'src' })
   .pipe(babel())
   .pipe(gulp.dest('build'))
+}
+
+/**
+ * functionPostCSS
+ * Returns gulp task for building stylesheets with PostCSS
+ *
+ * @param {String} [file] - path of the file to build with PostCSS
+ * @returns {Stream} - a gulp stream
+ */
+const functionPostCSS = (file) => {
+  const buildFiles = (file == null) ? ['src/**/*.css'] : file
+
+  return gulp.src(buildFiles, { base: 'src' })
+  .pipe(postcss([
+    cssnext({ browsers })
+  ]))
+  .pipe(gulp.dest('build'))
+}
+
+/**
+ * functionWebpack
+ * Returns gulp task for building client side webpack bundle
+ *
+ * @returns {String} - a gulp stream
+ */
+const functionWebpack = () => {
+  return gulp.src('src/houston/public/scripts/app.js')
+  .pipe(gulpWebpack(webpackConfig, webpack))
+  .pipe(gulp.dest('build/houston/public/scripts'))
 }
 
 /**
@@ -49,7 +90,7 @@ gulp.task('clean', () => {
  * build
  * Runs all build related tasks
  */
-gulp.task('build', ['build-copy', 'build-babel'])
+gulp.task('build', ['build-copy', 'build-babel', 'build-postcss', 'build-webpack'])
 
 /**
  * build-copy
@@ -64,6 +105,18 @@ gulp.task('build-copy', () => functionCopy())
 gulp.task('build-babel', () => functionBabel())
 
 /**
+ * build-postcss
+ * Builds all src/ stylesheets with PostCSS
+ */
+gulp.task('build-postcss', () => functionPostCSS())
+
+/**
+ * build-webpack
+ * Builds client side webpack bundle
+ */
+gulp.task('build-webpack', () => functionWebpack())
+
+/**
  * watch
  * Watches files for change
  */
@@ -71,10 +124,13 @@ gulp.task('watch', () => {
   gulp.watch('src/**/*', (obj) => {
     if (obj == null || obj.type !== 'changed') return
 
-    if (path.extname(obj.path) !== '.js') {
-      functionCopy(obj.path)
-    } else {
+    if (path.extname(obj.path) === '.css') {
+      functionPostCSS(obj.path)
+    } else if (path.extname(obj.path) === '.js') {
       functionBabel(obj.path)
+      functionWebpack()
+    } else {
+      functionCopy(obj.path)
     }
   })
 })
