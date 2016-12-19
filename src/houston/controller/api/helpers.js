1e65bb5e7faaee4e65a20043d70cb41f1272d6b2
Blake Kostner <btkostner@gmail.com>
2016-12-19 16:40:45 -0700

    add amountify helper and tests

--- src/houston/controller/api/helpers.js
+++ src/houston/controller/api/helpers.js
@@ -1,8 +1,11 @@
 /**
  * houston/controller/api/helpers.js
  * Some API endpoint helpers
+ * NOTE: all functions here throw unspecific errors. They should be wrapped in
+ * try blocks for more accurate and useful errors.
  *
  * @exports {Function} nameify - Takes a string and transforms it to a project name
+ * @exports {Function} amountify - Takes a string and returns an integer value
  */
 
 import { nameify as nameUpstream } from 'service'
@@ -12,6 +15,7 @@ import APIError from './error'
  * nameify
  * Takes a string and transforms it to a project name. Useful to standardize
  * input from endpoints.
+ * NOTE: wrap this in a try block to catch errors and rethrow more specific ones
  *
  * @param {String} str - a string to transform
  *
@@ -19,6 +23,10 @@ import APIError from './error'
  * @returns {String} - a RDNN string that may be a project name
  */
 export function nameify (str) {
+  if (str == null) {
+    throw new APIError(400, 'Invalid Project Name')
+  }
+
   const splits = str.split('.')
 
   if (splits.length < 3) {
@@ -26,4 +34,38 @@ export function nameify (str) {
   }
 
   return splits.map((s) => nameUpstream(s)).join('.')
+}
+
+/**
+ * amountify
+ * Takes a string and returns an integer value
+ * NOTE: wrap this in a try block to catch errors and rethrow more specific ones
+ *
+ * @param {String} num - a value to transform
+ *
+ * @throws {APIError} - when string is not a strict integer
+ * @returns {Number} - a integer value
+ */
+export function amountify (num) {
+  if (num == null || /\S/.test(num) === false) {
+    throw new APIError(400, 'Invalid Number')
+  }
+
+  let amount = 0
+
+  try {
+    amount = Number(num)
+  } catch (err) {
+    throw new APIError(400, 'Invalid Number')
+  }
+
+  if (isNaN(amount)) {
+    throw new APIError(400, 'Invalid Number')
+  }
+
+  if (amount % 1 !== 0) {
+    throw new APIError(400, 'Invalid Amount')
+  }
+
+  return amount
 }
--- test/houston/controller/api/helpers.js
+++ test/houston/controller/api/helpers.js
@@ -21,6 +21,20 @@ test('nameifies raw input', (t) => {
   t.is(t.context.helpers.nameify('com.github.elementary.houston'), 'com.github.elementary.houston')
   t.is(t.context.helpers.nameify('com.github.&j389%*#$&fj9vj02.j9fjv$@#%$203u0=-2f'), 'com.github.j389fj9vj02.j9fjv203u0-2f')
 
+  t.throws(() => t.context.helpers.nameify(null), t.context.APIError)
+  t.throws(() => t.context.helpers.nameify(''), t.context.APIError)
   t.throws(() => t.context.helpers.nameify('com'), t.context.APIError)
   t.throws(() => t.context.helpers.nameify('29fj23f.j922c3'), t.context.APIError)
+})
+
+test('amountifies raw input', (t) => {
+  t.is(t.context.helpers.amountify('123'), 123)
+  t.is(t.context.helpers.amountify('-123'), -123)
+  t.is(t.context.helpers.amountify('000001'), 1)
+
+  t.throws(() => t.context.helpers.amountify(null), t.context.APIError)
+  t.throws(() => t.context.helpers.amountify(''), t.context.APIError)
+  t.throws(() => t.context.helpers.amountify('923.23'), t.context.APIError)
+  t.throws(() => t.context.helpers.amountify('asdf'), t.context.APIError)
+  t.throws(() => t.context.helpers.amountify('-23.f3'), t.context.APIError)
 })
