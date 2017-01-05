33d7019023b40909056068ac19b4396957c07f36
Blake Kostner <btkostner@gmail.com>
2017-01-05 03:32:28 -0700

    add flow checking to service files

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,7 @@
 /**
  * service/index.js
  * Handles third party service integrations
+ * @flow
  *
  * @exports {Class} ServiceError - error relating to third party services
  * @exports {Function} nameify - turns a string into a RDNN compatible segment
@@ -10,16 +11,20 @@
  * ServiceError
  * a specific error relating to communication of third party services
  *
+ * @property {String} code - an specific code to check error references
+ *
  * @extends Error
  */
 export class ServiceError extends Error {
+
+  code: string
 
   /**
    * Creates a new serviceError
    *
    * @param {String} msg - message to put on the error
    */
-  constructor (msg) {
+  constructor (msg: string) {
     super(msg)
     Error.captureStackTrace(this, this.constructor)
 
@@ -36,7 +41,7 @@ export class ServiceError extends Error {
  * @param {String} str - string to transform
  * @returns {String} - a RDNN compatible segment
  */
-export function nameify (str) {
+export function nameify (str: string): string {
   return str
   .toLowerCase()
   .replace(/(\s|_|\.)+/gmi, '-')
