06269101df767ff934d751c68a1b73640baa4b9b
Blake Kostner <btkostner@gmail.com>
2017-04-14 10:32:04 -0700

    remove if checks to hopefully remove race condition (#407)
    * remove if checks to hopefully remove race condition
    
    * add back if check to avoid back-statusing

--- src/houston/controller/hook/flightcheck.js
+++ src/houston/controller/hook/flightcheck.js
@@ -21,13 +21,10 @@ worker.register('start', async (param) => {
   const cycle = await Cycle.findById(param.id)
   const status = await cycle.getStatus()
 
-  if (status !== 'QUEUE') {
-    log.debug('Received flightcheck start data for a cycle already checked')
-    return
+  if (status === 'QUEUE') {
+    log.debug('Received flightcheck data for start cycle')
+    await cycle.setStatus('RUN')
   }
-
-  log.debug('Received flightcheck data for start cycle')
-  await cycle.setStatus('RUN')
 })
 
 /**
@@ -41,12 +38,6 @@ worker.register('start', async (param) => {
  */
 worker.register('finish', async (param) => {
   const cycle = await Cycle.findById(param.id)
-  const status = await cycle.getStatus()
-
-  if (status !== 'RUN') {
-    log.debug('Received flightcheck finish data for a cycle not running')
-    return
-  }
 
   log.debug(`Received flightcheck data for finish cycle ${param.id}`)
   log.debug(`Found ${param.logs.length} logs in ${cycle.name}`)
@@ -93,14 +84,8 @@ worker.register('finish', async (param) => {
  */
 worker.register('error', async (param) => {
   const cycle = await Cycle.findById(param.id)
-  const status = await cycle.getStatus()
 
-  if (status !== 'RUN') {
-    log.debug('Received flightcheck error data for a cycle not running')
-    return
-  } else {
-    log.debug('Received flightcheck data for error cycle')
-  }
+  log.debug('Received flightcheck data for error cycle')
 
   await cycle.setStatus('ERROR')
   await cycle.update({ mistake: param.error })
