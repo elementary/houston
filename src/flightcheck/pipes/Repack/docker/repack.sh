f25cad9ae4971b34086984d04ec710bdc3e89a73
Blake Kostner <btkostner@gmail.com>
2017-09-18 17:56:10 -0700

    chmod everything after unpacking deb file (#540)

--- src/flightcheck/pipes/Repack/docker/repack.sh
+++ src/flightcheck/pipes/Repack/docker/repack.sh
@@ -25,16 +25,39 @@ if [ "$O" == "pack" ] && [ ! -d "$P" ]; then
   exit 1
 fi
 
-# Extracting a package
 if [ "$O" == "extract" ]; then
+  cd $P
+
+  # Extracting a package
   dpkg-deb -x $P $D
   dpkg-deb -e $P $D/DEBIAN
 
   rm $P
+
+  # Creates a list of all the original package permissions for each file
+  touch $D/FILES
+  find $P -exec stat -c "%a;%n" {} \; > $D/FILES
+
+  # Sets everything to an open permission to allow edits without root access
+  chmod -R 777 $D
 fi
 
 if [ "$O" == "pack" ]; then
   cd $P
+
+  # Restores the original package permissions
+  while read l in; do
+    IFS=';' read o f <<< "$l"
+
+    chmod "$o" "$f"
+  done < $P/FILES
+
+  # Remove any extra files that are not part of the package
+  rm $P/FILES
+
+  # And build the package
+  mkdir -p $P/DEBIAN
+  touch $P/DEBIAN/md5sums
   find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > $P/DEBIAN/md5sums
   dpkg-deb -b $P $D
 
