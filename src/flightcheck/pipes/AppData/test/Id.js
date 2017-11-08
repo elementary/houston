e7bdd191bb10e83c89d2994a378822ad65ef4a5c
David Howell <davidh@dynamicmethods.com.au>
2017-11-09 09:10:53 +1030

    Added basic check for appdata id

---
+++ src/flightcheck/pipes/AppData/test/Id.js
@@ -0,0 +1,43 @@
+/**
+ * flightcheck/pipes/AppData/test/changelog.js
+ * Checks appdata has id key and matches the RDNN
+ * @flow
+ *
+ * @exports {Pipe} - Checks appdata has id key and matches the RDNN
+ */
+
+import Parseable from 'flightcheck/file/parsable'
+import Pipe from 'flightcheck/pipes/pipe'
+
+/**
+ * AppDataChangelog
+ * Checks appdata has id key and matches the RDNN
+ *
+ * @extends Pipe
+ */
+export default class AppDataId extends Pipe {
+
+  /**
+   * code
+   * Checks appdata has id key and matches the RDNN
+   *
+   * @param {Parseable} f - The AppData file
+   * @returns {Void}
+   */
+  async code (f: Parseable) {
+    const file = await f.parse()
+    const expectedId = `${this.pipeline.build.name}.desktop`;
+
+    try {
+      if (!file.component.id) {
+        throw new Error('Missing id');
+      }
+
+      if (file.component.id[0] !== expectedId) {
+        throw new Error(`Id [${file.component.id[0]}] does not match expected RDNN [${expectedId}]`);
+      }
+    } catch (err) {
+      return this.log('warn', 'AppData/test/id.md');
+    }
+  }
+}
---
+++ src/flightcheck/pipes/AppData/test/id.md
@@ -0,0 +1,6 @@
+Missing AppStream id
+
+AppCenter requires the AppStream id to provided and must match you RDNN. Please provide an id for your releases.
+
+For more information, please look at the
+[appstream documentation](https://www.freedesktop.org/software/appstream/docs/chap-Quickstart.html).
