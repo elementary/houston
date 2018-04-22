d90110ae1ba7f9bb5b37bb39337af6d96d1e477c
Blake Kostner <btkostner@gmail.com>
2018-04-21 21:59:14 -0700

    globally set test timeout higher

---
+++ test/bootstrap.js
@@ -0,0 +1,7 @@
+/**
+ * houston/test/bootstrap.js
+ * Runs before all tests. Sets things up.
+ */
+
+// Our tests can get pretty dang complex. Up the timeout to make travis happy.
+jest.setTimeout(70000)
