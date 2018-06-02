61a8480af555aa94509ef1f5cc5117284b89e718
Blake Kostner <btkostner@gmail.com>
2018-06-02 16:17:54 -0700

    fix: fix app export for creating repositories

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ export { App } from './lib/app'
 
 export { Config } from './lib/config'
 
-export { codeRepository } from './lib/service'
+export { createCodeRepository } from './lib/service'
 
 export { Worker } from './worker'
 export { Build as BuildWorker } from './worker'
